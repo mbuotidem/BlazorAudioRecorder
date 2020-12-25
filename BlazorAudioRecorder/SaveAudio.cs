1b56eed574f5485bac945710d0202f03d0aec6a0
Mbuotidem Isaac <mbuotidemisaac@gmail.com>
2020-12-24 23:30:09 -0500

    Add project files.

---
+++ BlazorAudioRecorder/SaveAudio.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAudioRecorder
+{
+    public class SaveAudio: Controller
+    {
+        Microsoft.AspNetCore.Hosting.IWebHostEnvironment _hostingEnvironment;
+
+        public SaveAudio(Microsoft.AspNetCore.Hosting.IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+
+        }
+
+        [Route("api/[controller]/Save")]
+        [HttpPost]
+        public async Task<IActionResult> Save(IFormFile file)
+        {
+            if (file.ContentType != "audio/wav")
+            {
+                return BadRequest("Wrong file type");
+            }
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");//uploads where you want to save data inside wwwroot
+
+            //var filePath = Path.Combine(uploads, Path.GetRandomFileName() + ".wav");
+            var filePath = Path.Combine(uploads, file.FileName + ".wav");
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            return Ok("File uploaded successfully");
+        }
+    }
+}
